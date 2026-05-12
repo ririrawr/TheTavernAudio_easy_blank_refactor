1e754ae8b77a048f28219d0974022b150baa05de
Aurora <aurorasycylia@gmail.com>
2026-05-12 19:43:13 +0200

    part 3

--- Assets/Scripts/Outside_foot_switch.cs
+++ Assets/Scripts/Outside_foot_switch.cs
@@ -44,7 +44,7 @@ public class Outside_foot_switch : MonoBehaviour
                 ToggleSnapshot(true);
             }
             // Wyłącza snapshot, jeśli gracz jest wewnątrz, a snapshot jest aktywny.
-            else if ((tag == "Inside_stone" || tag == "Inside_wood") && snapshotActivated)
+            else if ((tag == "Inside_stone" || tag == "Wood") && snapshotActivated)
             {
                 ToggleSnapshot(false);
             }
